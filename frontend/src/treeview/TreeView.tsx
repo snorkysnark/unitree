196c3b9ad70afa12d00cb93150da7b36e5dcadb1
snorkysnark <snorkysnark@gmail.com>
2024-07-08 16:26:30 +0200

    simple indentlines

--- frontend/src/treeview/TreeView.tsx
+++ frontend/src/treeview/TreeView.tsx
@@ -4,12 +4,21 @@ export default function TreeView({ nodes }: { nodes: NodeOut[] }) {
   return (
     <div>
       {nodes.map((node) => (
-        <div
-          key={node.id}
-          style={{ marginLeft: `${node.depth * 2}rem` }}
-          className="hover:bg-gray-100 h-8 px-2 align-middle select-none text-nowrap"
-        >
-          {node.title}
+        <div key={node.id} className="relative">
+          <div
+            style={{ marginLeft: `${node.depth * 2}rem` }}
+            className="hover:bg-gray-100 h-8 px-2 align-middle select-none text-nowrap"
+          >
+            {Array.from({ length: node.depth }, (_, i) => i).map((depth) => (
+              <div
+                key={depth}
+                className="absolute h-full border-solid border-2 border-l-gray-400"
+                style={{ left: `${depth * 2 + 1}rem` }}
+              />
+            ))}
+
+            {node.title}
+          </div>
         </div>
       ))}
     </div>
