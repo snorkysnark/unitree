b97715f9fbfb38b6e9d08e7168b361be7068f244
snorkysnark <snorkysnark@gmail.com>
2024-08-17 01:38:14 +0200

    open browser

--- unitree/cli.py
+++ unitree/cli.py
@@ -1,12 +1,26 @@
 import typer
 import uvicorn
+import webbrowser
+from multiprocessing import Process
 
 from .app import app
 
 
-def serve_cli(port: int = 8000):
+def open_browser(port: int):
+    webbrowser.open(f"http://localhost:{port}")
+
+
+def serve(port: int = 8000):
+    browser = Process(target=open_browser, args=(port,))
+    browser.start()
+
     uvicorn.run(app, port=port)
+    browser.join()
 
 
-def serve():
-    typer.run(serve_cli)
+def main():
+    typer.run(serve)
+
+
+if __name__ == "__main__":
+    main()
